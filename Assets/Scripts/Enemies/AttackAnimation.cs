ae9004a939c407478ffc0bc0f1f36ec602655a7e
Sofia Vanegas Cordoba <65092315+Sofito-code@users.noreply.github.com>
2025-01-16 02:05:17 -0500

    Activar ataque de enemigo 1 mediante codigo

--- Assets/Scripts/Enemies/AttackAnimation.cs
+++ Assets/Scripts/Enemies/AttackAnimation.cs
@@ -1,19 +1,14 @@
-using System;
 using UnityEngine;
 
 public class AttackAnimation : MonoBehaviour
 {
     public GameObject bubble;
-    public GameObject cloud;
     public Transform spawnPointBubble;
-    public Transform spawnPointCloud;
     private GameObject bubbleRef;
-    private GameObject cloudRef;
     private Rigidbody2D rb;
 
     void throwBubble()
     {
-        print("Lanzar burbuja");
         bubbleRef = Instantiate(bubble, spawnPointBubble);
         rb = bubbleRef.GetComponent<Rigidbody2D>();
         Vector2 direction = new Vector2(3.5f,1.5f);
@@ -22,9 +17,7 @@ public class AttackAnimation : MonoBehaviour
 
     void throwCloud()
     {
-        print("Lanzar nube");
-        Destroy(bubbleRef);
-        cloudRef = Instantiate(cloud, spawnPointCloud);
-        
+        rb.linearVelocity = Vector2.zero;
+        rb.gravityScale = 0;
     }
 }
