81b6698b81977e895e667e489b4ce598821131da
RickContreras <ricardo.contreras568@hotmail.com>
2025-01-23 01:17:19 -0500

    Se trabajo en el final del boss

--- Assets/Scripts/Bosses/BossHealth.cs
+++ Assets/Scripts/Bosses/BossHealth.cs
@@ -1,13 +1,16 @@
+using Assets.Scripts.Bosses.Mago;
 using UnityEngine;
+using Assets.Scripts.Bosses;
 
-namespace Assets.Scripts.Bosses
-{
+
+
+
     public class BossHealth : MonoBehaviour
     {
         public int maxHealth = 100;
         private int currentHealth;
-        [SerializeField]private Animator mago;
-        [SerializeField]private Animator cientifico;
+        [SerializeField]private GameObject mago;
+        [SerializeField]private Cientifico cientifico;
 
         private void Start()
         {
@@ -27,8 +30,12 @@ namespace Assets.Scripts.Bosses
 
         private void Die()
         {
+            Animator animago = mago.transform.Find("Skin").GetComponent<Animator>();
+            mago.GetComponent<Collider2D>().enabled = false;
+            animago.SetTrigger("Died");
+            mago.GetComponent<Mago>().enabled = false;
+            cientifico.isStarted = false;
             // Implementar lógica de muerte aquí
             Debug.Log("Boss muerto");
         }
     }
-}
