42de30afe8985ee689d89aefe4bbd701ed70e8a5
Sofia Vanegas Cordoba <65092315+Sofito-code@users.noreply.github.com>
2025-01-24 02:12:43 -0500

    Ajuste de video intro

---
+++ Assets/IntroVideoManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Video;
+
+public class IntroVideoManager : MonoBehaviour
+{
+    private VideoPlayer vp;
+
+    public GameObject canvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        float duration = (float) vp.length;
+        StartCoroutine(ClipFinished(duration - 10f));
+    }
+
+    IEnumerator ClipFinished(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        canvas.GetComponent<FlowManager>().GoWithLoading("Guia Inicial");
+    }
+}
